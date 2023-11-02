d7e711f56731a61f3c63f7cdfb66f221f65fd14a
Kirill213sdd1 <144334960+Kirill213sdd1@users.noreply.github.com>
2023-11-02 18:53:29 +0300

    Add files via upload

---
+++ 02.11.23-main/number3.py
@@ -0,0 +1,14 @@
+class Test:
+
+    def __repr__(self):
+        return 'mmgkkg'
+
+    def __str__(self):
+        return '34246262'
+
+    def __bool__(self):
+        return True
+
+test = Test()
+if test:
+    print('dfgdgfmuuuu')
